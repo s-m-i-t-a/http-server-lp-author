a77495216e1a63dfbf12595516933e86e430f699
Jindrich K. Smitka <jsmitka@smita.info>
2024-09-23 11:47:07 +0200

    Changed to print only selected lang

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,9 +1,11 @@
 #[derive(Debug)]
+#[derive(PartialEq)]
 enum Lang {
-  English,
-  Spanish,
-  Chinese,
-  Texan,
+    English,
+    Spanish,
+    Chinese,
+    Texan,
+    Czech,
 }
 
 struct Greeting {
@@ -12,18 +14,37 @@ struct Greeting {
 }
 
 fn main() {
-  let mut v :Vec<Greeting> = Vec::new();
+    let mut v: Vec<Greeting> = Vec::new();
 
-  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
-  v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::English,
+        message: String::from("Hello WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Spanish,
+        message: String::from("Hola WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Texan,
+        message: String::from("Howdy WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Chinese,
+        message: String::from("WasmEdge 你好!"),
+    };
+    v.push(g);
 
-  for e in v {
-    println!("{:?} {}", e.lang, e.message);
-  }
+    v.push(Greeting {
+        lang: Lang::Czech,
+        message: String::from("Ahoj WasmEdge!"),
+    });
+
+    for e in v {
+        if e.lang == Lang::Czech {
+            println!("{:?} {}", e.lang, e.message);
+        }
+    }
 }
